ec0aeb97c7ac63d4c297e7148def8fe8232daf90
Inder <112728880+inderjz@users.noreply.github.com>
2023-01-14 09:56:39 -0800

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   require('dotenv').config();
-  sequelize = new Sequelize(process.env.DB_NAME = 'ecommerce_db', process.env.DB_USER = 'root', process.env.DB_PW = 'Livnoor11', {
+  sequelize = new Sequelize(process.env.DB_NAME = '', process.env.DB_USER = '', process.env.DB_PW = '', {
     host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306,
